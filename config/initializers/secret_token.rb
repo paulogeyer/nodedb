25dfcb3ed8265864412c24f0e21c6847f93d70ec
Paulo Geyer <paulogeyer@gmail.com>
2013-06-29 12:27:59 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nodedb::Application.config.secret_key_base = 'dc3f1c59a08a8585b72f3e050a53bcaba9fc8acd15e8da669648f603e6efb82fe4c4a00b427cbe9c6408a7d5634760f5223083426dd4055484ad4bcf93261705'
